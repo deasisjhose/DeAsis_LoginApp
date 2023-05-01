01fb875e3ab08e752ebc15bb633dd1218b91d19c
deasisjhose <jhose_mhiguel_deasis@dlsu.edu.ph>
2023-05-01 23:44:05 +0800

    Add project files.

---
+++ DeAsis_LoginApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DeAsis_LoginApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
